681b23be249315bb541549d06f9b772dfdfd58c2
leosouto26620-rgb <leosouto26620@gmail.com>
2026-01-05 23:56:48 -0300

    Add Playwright and CI

---
+++ playwright.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+
+  reporter: [["html", { open: "never" }]],
+
+  use: {
+    headless: !!process.env.CI,
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
+    trace: "on-first-retry",
+    baseURL: "https://www.saucedemo.com/",
+  },
+
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+});
